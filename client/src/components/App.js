2a44b25accb8a3dda0b491a6979641c7daf63fc8
michael mirhom <micharby73@gmail.com>
2023-09-18 07:14:26 -0500

    completed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -56,4 +56,26 @@ function App() {
   );
 }
 
-export default App;
+function NewMessage({ currentUser, onAddMessage }) {
+  const [body, setBody] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    fetch("http://127.0.0.1:5555/messages", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: currentUser.username,
+        body: body,
+      }),
+    })
+      .then((r) => r.json())
+      .then((newMessage) => {
+        onAddMessage(newMessage);
+        setBody("");
+      });
+  }
+  
